f49fd8f5c209cab09dabc581f7cb52e15f520cdf
Annie Katz <91238138+anniekatz@users.noreply.github.com>
2022-12-03 22:12:02 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -12,34 +12,19 @@ local leader = ","
 -- Set the local leader key
 local local_leader = ";"
 
--- Set the shell
-local shell = os.getenv("SHELL")
-
 -- Set the colorscheme
 local colorscheme = "gruvbox"
 
--- Set the font
+-- Set font
 local font = "Hack Nerd Font"
-
--- Set the font size
 local font_size = 12
 
--- Set the line numbers
+-- Set formatting
 local line_numbers = true
-
--- Set the auto indent
 local auto_indent = true
-
--- Set the tab width
 local tab_width = 4
-
--- Set the soft wrap
 local soft_wrap = true
 
--- Set the plugins
-local plugins = {
-  --set plugins here
-}
 
 vim.g.lua_tree_indent_size = 2
 vim.g.lua_tree_indent_char = " "
